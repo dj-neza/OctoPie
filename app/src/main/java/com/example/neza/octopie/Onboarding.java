c560154ffd45606efd2fe570a9948ca418ae69aa
Neza <neza@dhcp-86-139.eduroam.aalto.fi>
2018-11-24 17:53:38 +0100

    no frag

--- app/src/main/java/com/example/neza/octopie/Onboarding.java
+++ app/src/main/java/com/example/neza/octopie/Onboarding.java
@@ -9,33 +9,14 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-public class Onboarding extends FragmentActivity {
-
-    private static final int NUM_PAGES = 4;
-    private ViewPager mPager;
-    private PagerAdapter mPagerAdapter;
+public class Onboarding extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_onboarding);
 
-        NormalFr frg=new NormalFr();//create the fragment instance for the top fragment
-        Fit frg1=new Fit();//create the fragment instance for the middle fragment
-        Vegan frg2=new Vegan();//create the fragment instance for the bottom fragment
-        Allergy frg3 = new Allergy();
-
-        FragmentManager manager=getSupportFragmentManager();//create an instance of fragment manager
-
-        FragmentTransaction transaction=manager.beginTransaction();//create an instance of Fragment-transaction
-
-        transaction.add(R.id.nm_frag, frg);
-        transaction.add(R.id.fit_frag, frg1);
-        transaction.add(R.id.veg_frag, frg2);
-        //transaction.add(R.id.all_frag, frg3);
-
-
-        transaction.commit();
     }
+
 
 }
