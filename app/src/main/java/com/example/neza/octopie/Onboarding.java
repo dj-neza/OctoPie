45df21200295eacb2fb37ef33afe787170ae5e60
Neza <neza@dhcp-86-139.eduroam.aalto.fi>
2018-11-24 19:54:14 +0100

    sharedPref

--- app/src/main/java/com/example/neza/octopie/Onboarding.java
+++ app/src/main/java/com/example/neza/octopie/Onboarding.java
@@ -1,5 +1,8 @@
 package com.example.neza.octopie;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -8,6 +11,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Onboarding extends AppCompatActivity {
 
@@ -15,8 +21,33 @@ public class Onboarding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+        defaultOcto();
 
+        final Button button = findViewById(R.id.checkPref);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences sp = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+                int lala = sp.getInt("octoId", 0);
+                final TextView t = findViewById(R.id.text1);
+                t.setText(String.valueOf(lala));
+            }
+        });
+
+        final Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), Recipes.class);
+                startActivity(i);
+            }
+        });
     }
 
+    public static final String MY_PREFS_NAME = "MyPref";
 
+    public void defaultOcto() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE); // 0 - for private mode
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("octoId", 1);
+        editor.apply();
+    }
 }
